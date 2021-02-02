f312d614426bb72162916a2fc11bfd5571d4f52d
RedSeal9 <30299681+RedSeal9@users.noreply.github.com>
2021-02-02 08:47:30 -0500

    new http client

--- src/main/java/red/mchttp/McHTTP.java
+++ src/main/java/red/mchttp/McHTTP.java
@@ -1,21 +1,28 @@
 package red.McHTTP;
 
-import org.bukkit.plugin.java.JavaPlugin;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
- 
+import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
-import org.apache.http.client.HttpClient;
+import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.entity.BasicHttpEntity;
+import org.apache.http.entity.BufferedHttpEntity;
+import org.apache.http.entity.HttpEntityWrapper;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.message.BasicNameValuePair;
 
-//public class McHTTP {
+import org.bukkit.plugin.java.JavaPlugin;
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
 public final class McHTTP extends JavaPlugin {
 
 String version = "1.0";
@@ -24,45 +31,38 @@ String pwrstate = "on";
 
 String srvname = "red";
 
-private final String USER_AGENT = "Mozilla/5.0";
+String USER_AGENT = "Mozilla/5.0";
 
-	public static void main(String[] args) throws Exception {
- 
-	    McHTTP http = new McHTTP();
- 
-	    System.out.println("GET Request Using HttpURLConnection");
-            http.sendGet();
-            System.out.println();
- 
-	}
- 
-	private void sendGet() throws Exception {
- 
-	    String username = "hitenpratap";
-            StringBuilder url = new stringBuilder("https://twitter.com/search");
-            stringBuilder.append("?q=");
-            stringBuilder.append(URLEncoder.encode(username, "UTF-8"));
- 
-		HttpClient client = new DefaultHttpClient();
-		HttpGet request = new HttpGet(url);
- 
-		request.addHeader("User-Agent", USER_AGENT);
-		HttpResponse response = client.execute(request);
- 
-		System.out.println("\nSending 'GET' request to URL : " + url);
-		System.out.println("Response Code : " + 
-                       response.getStatusLine().getStatusCode());
- 
-		BufferedReader rd = new BufferedReader(
-                       new InputStreamReader(response.getEntity().getContent()));
- 
-		StringBuffer result = new StringBuffer();
-		String line = "";
-		while ((line = rd.readLine()) != null) {
-			result.append(line);
-		}
- 
-		System.out.println(result.toString());
- 
-	}
+String url = "https://redseal.red";
+    public void sendGETRequestv2(final String url) {
+
+        //Build client
+        try (final CloseableHttpClient httpClient = HttpClientBuilder.create().setUserAgent(USER_AGENT).build()) {
+
+            final HttpGet request = new HttpGet();
+            request.setURI(new URL(url).toURI());
+
+            final HttpResponse response = httpClient.execute(request);
+            final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader((response.getEntity().getContent())));
+
+            int responseCode = response.getStatusLine().getStatusCode();
+            System.out.println("Send http GET request...(" + url + ")");
+            System.out.println("Request status..." + responseCode);
+
+            String bufferReaderLine;
+            final StringBuilder stringBuilder = new StringBuilder();
+            while ((bufferReaderLine = bufferedReader.readLine()) != null) {
+                stringBuilder.append(bufferReaderLine);
+            }
+
+            bufferedReader.close();
+            System.out.print(response);
+
+            System.out.println("\nRequest completed successfully.");
+        } catch (final IOException ioE) {
+            ioE.printStackTrace();
+        } catch (final URISyntaxException e) {
+            e.printStackTrace();
+        }
+    }
 }
